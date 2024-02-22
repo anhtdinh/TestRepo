9767bdb49f36f8efb7e67ec4cf5d1ad9b350deca
anhtdinh <anhtdinh@gmail.com>
2024-02-23 09:20:57 +1000

    Create rf1.R

---
+++ rf1.R
@@ -0,0 +1,17 @@
+# ---- Installing package ----
+install.packages("dplyr")
+install.packages("tinytex")
+install.packages("tidyverse")
+installed.packages("ggplot2")
+install.packages("randomForest")
+
+# ---- Loading packages ----
+library(dplyr)
+library(tinytex)
+library(tidyverse)
+library(ggplot2)
+library(randomForest)
+
+
+# ---- Loading dataset -----
+
