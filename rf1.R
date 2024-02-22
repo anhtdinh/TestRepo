b9c49269bc3f87320d8d77c30dd0cb410746f43c
anhtdinh <anhtdinh@gmail.com>
2024-02-23 09:35:02 +1000

    Update rf1.R

--- rf1.R
+++ rf1.R
@@ -4,6 +4,7 @@ install.packages("tinytex")
 install.packages("tidyverse")
 installed.packages("ggplot2")
 install.packages("randomForest")
+install.packages("googlesheets4")
 
 # ---- Loading packages ----
 library(dplyr)
@@ -11,7 +12,14 @@ library(tinytex)
 library(tidyverse)
 library(ggplot2)
 library(randomForest)
+library(googlesheets4)
 
 
 # ---- Loading dataset -----
 
+data_train <- read.csv("https://raw.githubusercontent.com/guru99-edu/R-Programming/master/train.csv")
+glimpse(data_train)
+data_test <- read.csv("https://raw.githubusercontent.com/guru99-edu/R-Programming/master/test.csv") 
+glimpse(data_test)
+ 
+# -----
