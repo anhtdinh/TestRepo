3fff5c0ce7a4163a6a79002db31961c693a2bcb6
anhtdinh <anhtdinh@gmail.com>
2024-02-23 10:17:48 +1000

    Update rf1.R

--- rf1.R
+++ rf1.R
@@ -5,6 +5,11 @@ install.packages("tidyverse")
 installed.packages("ggplot2")
 install.packages("randomForest")
 install.packages("googlesheets4")
+install.packages("rsample")
+install.packages("rpart")
+install.packages("rpart.plot")
+install.packages("ipred")
+install.packages("caret")
 
 # ---- Loading packages ----
 library(dplyr)
@@ -13,6 +18,12 @@ library(tidyverse)
 library(ggplot2)
 library(randomForest)
 library(googlesheets4)
+
+library(rsample)     # data splitting 
+library(rpart)       # performing regression trees
+library(rpart.plot)  # plotting regression trees
+library(ipred)       # bagging
+library(caret)       # bagging
 
 
 # ---- Loading dataset -----
