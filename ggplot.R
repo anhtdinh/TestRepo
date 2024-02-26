b241e5d6d7141f4b1b2a99c836829229c0e5d4f0
anhtdinh <anhtdinh@gmail.com>
2024-02-26 10:22:47 +1000

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -1,10 +1,22 @@
+# -----------
+
 data = mtcars
 
 
 data %>% 
   ggplot(aes(wt,mpg,color = as.factor(gear), shape = as.factor(cyl), size = hp))+
   geom_point(alpha =0.6)+
-  scale_color_manual(values = c("#3a0cc3", "#c9184a", "#3a5a20"))
+  scale_color_manual(values = c("#3a0cc3", "#c9184a", "#3a5a20")) + 
+  geom_smooth(wt,mpg)
 
 
 
+# -------
+install.packages("MASS")
+library(MASS)
+data("Boston")
+
+boston_data_long <- Boston %>%
+  pivot_longer(cols = everything(),
+               names_to = "variable",
+               values_to = "value")
