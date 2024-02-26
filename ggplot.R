5d2c58f533cb6a8cc65b3e7a3b7752d49383544f
anhtdinh <anhtdinh@gmail.com>
2024-02-26 10:38:09 +1000

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -20,3 +20,17 @@ boston_data_long <- Boston %>%
   pivot_longer(cols = everything(),
                names_to = "variable",
                values_to = "value")
+
+boston_histograms <- ggplot(boston_data_long, aes(x = value)) +
+  geom_histogram(bins = 30, color = "black", fill = "lightblue") +
+  facet_wrap(~variable,scale = "free", ncol = 4) +
+  labs(title = "Histograms of Numeric Variables in the Boston Housing Dataset",
+       x = "Value",
+       y = "Frequency") +
+  theme_minimal()
+
+# Plot the histograms
+print(boston_histograms)
+
+
+
